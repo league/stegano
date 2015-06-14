cbb42ade8c9d898886c8fbc95440cd6fbf95d91c
Christopher League <league@contrapunctus.net>
2015-06-14 19:33:37 -0400

    Randomize remaining bits in encode-file

--- encode-file.cpp
+++ encode-file.cpp
@@ -14,17 +14,20 @@ struct bufstate {
 
 Quantum mergeNextTwoBits (ifstream& in, bufstate& bs, Quantum q)
 {
+    Quantum result;
     if(bs.bitsLeft == 0) {
         if(in.get(bs.byte)) {
             // cout << "Read byte: " << bitset<8>(bs.byte) << '\n';
             bs.bitsLeft = 8;
         }
         else {
-            return 0;
+            result = rand() % 4;
         }
     }
-    bs.bitsLeft -= 2;
-    Quantum result = (bs.byte & (3 << bs.bitsLeft)) >> bs.bitsLeft;
+    if(bs.bitsLeft > 0) {
+        bs.bitsLeft -= 2;
+        result = (bs.byte & (3 << bs.bitsLeft)) >> bs.bitsLeft;
+    }
     result = (result << 8) | result;
     Quantum merged = (q & ~0x0303) | result;
     // cout << "Next two: " << bitset<2>(result) << " merged into "
@@ -35,6 +38,7 @@ Quantum mergeNextTwoBits (ifstream& in, bufstate& bs, Quantum q)
 int main(int argc, char** argv)
 {
     InitializeMagick(*argv);
+    srand(time(NULL));
     if(argc != 4) {
         cout << "Usage: " << argv[0] << " input.png secret.data output.png\n";
         exit(1);
@@ -61,6 +65,7 @@ int main(int argc, char** argv)
             c.greenQuantum(mergeNextTwoBits(in, bs, c.greenQuantum()));
             c.blueQuantum(mergeNextTwoBits(in, bs, c.blueQuantum()));
             im.pixelColor(x, y, c);
+            // Uncomment to leave remaining bits alone
             //if(!in) goto done;
         }
     }
