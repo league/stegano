fc92236d01efb53e144902c1e0742b86c310223c
Christopher League <league@contrapunctus.net>
2015-06-14 19:33:03 -0400

    Leading 0 in hex

--- spy.cpp
+++ spy.cpp
@@ -24,9 +24,9 @@ int main(int argc, char** argv)
       Image im (argv[i]);
       Color c = im.pixelColor(x, y);
       cout << setw(20) << argv[i] << ": "
-           << setw(2) << hex << (c.redQuantum() & 0xff)
-           << setw(2) << hex << (c.greenQuantum() & 0xff)
-           << setw(2) << hex << (c.blueQuantum() & 0xff)
+           << setfill('0') << setw(2) << hex << (c.redQuantum() & 0xff)
+           << setfill('0') << setw(2) << hex << (c.greenQuantum() & 0xff)
+           << setfill('0') << setw(2) << hex << (c.blueQuantum() & 0xff)
            << "  ";
       bitset<8> r(c.redQuantum());
       bitset<8> g(c.greenQuantum());
